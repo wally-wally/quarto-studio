6d5c9b7772d2aafb01fb967a2c2f7a31226201ef
Wally(심규현) <sgh@gabia.com>
2026-06-25 11:46:38 +0900

    chore: 로컬 Python 가상환경 린트 제외

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
-  { ignores: [".next/**", "node_modules/**", "coverage/**"] },
+  { ignores: [".next/**", "node_modules/**", "coverage/**", ".venv/**"] },
   ...nextVitals,
   ...nextTypescript
 ];
